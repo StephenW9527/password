c94e27f73c5b24ce99e82aab5e56dd5251b8270e
Stephen Wei <log9527@gmail.com>
2025-07-10 17:50:58 +0800

    first commit

---
+++ password.py
@@ -0,0 +1,26 @@
+
+
+pw = 'a123456'
+print('請輸入密碼：')
+test = input()
+times = 3
+
+while True:
+    if test == pw:
+
+        print('登入成功')
+        break
+    else:
+        times = times - 1
+        if times == 0:
+
+            print("已鎖卡 請洽發卡銀行")
+            break
+
+        else:
+
+            print('密碼錯誤! 還有', times, '次機會')
+            print('請重新輸入：')
+            test = input()
+
+            continue
