41758e43061d91be999cc11a5fba5887230d2c49
Stephen Wei <log9527@gmail.com>
2025-07-10 18:22:05 +0800

    加入老師寫法

--- password.py
+++ password.py
@@ -1,21 +1,31 @@
 
 
 pwd = 'a123456'
-print('請輸入密碼：')
-test = input()
+# test = input('請輸入密碼：')
 times = 3
 
-while True:
+# while True:
+#     if test == pwd:
+#         print('登入成功')
+#         break
+#     else:
+#         times = times - 1
+#         if times == 0:
+#             print("已鎖卡 請洽發卡銀行")
+#             break
+#         else:
+#             print('密碼錯誤! 還有', times, '次機會')
+#             print('請重新輸入：')
+#             test = input()
+#             continue
+
+
+# 老師寫法:
+while times > 0:
+    test = input('請輸入密碼：')
     if test == pwd:
         print('登入成功')
         break
     else:
         times = times - 1
-        if times == 0:
-            print("已鎖卡 請洽發卡銀行")
-            break
-        else:
-            print('密碼錯誤! 還有', times, '次機會')
-            print('請重新輸入：')
-            test = input()
-            continue
+        print('密碼錯誤! 還有', times, '次機會')
